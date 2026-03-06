1b5ed2300b8a5c8a14e56249830fe999aa4262b8
InfoLab <moiznmaiz@gmail.com>
2026-03-07 04:01:24 +0500

    Update generate_sitemap.py

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -1,13 +1,28 @@
 import os
 
-base_url = "https://onetouch12345.github.io//"
+base_url = "https://onetouch12345.github.io/forex-education-site/"
 
-pages = [f for f in os.listdir() if f.endswith(".html")]
+# Define important pages only
+pages = [
+    "index.html",
+    "learn.html",
+    "analysis.html",
+    "blog.html",
+    "tools.html",
+    "brokers.html",
+    "contact.html"
+]
 
-with open("sitemap.xml", "w") as f:
+with open("sitemap.xml", "w", encoding="utf-8") as f:
+
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+
     for page in pages:
-        f.write(f"<url><loc>{base_url}{page}</loc></url>\n")
+        f.write("<url>\n")
+        f.write(f"<loc>{base_url}{page}</loc>\n")
+        f.write("<changefreq>weekly</changefreq>\n")
+        f.write("<priority>0.8</priority>\n")
+        f.write("</url>\n")
 
     f.write("</urlset>")
